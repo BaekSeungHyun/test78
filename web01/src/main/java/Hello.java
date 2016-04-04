8fa0171398e95c734b57cd6b71335618ff156054
BaekSeungHyun <bsh2369@gmail.com>
2016-04-04 17:18:09 +0900

    .

--- web01/src/main/java/Hello.java
+++ web01/src/main/java/Hello.java
@@ -2,5 +2,11 @@ public class Hello {
   public static void main(String[] args) {
 	System.out.println("okok");
 	System.out.println("dd");
+	System.out.println("calculate a+b ::");
+	int a= 20;
+	int b= 10;
+	
+	int c=a+b;
+	System.out.println(c+"==>done!");
   }
 }
