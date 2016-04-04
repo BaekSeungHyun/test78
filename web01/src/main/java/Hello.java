719e6577bb53dfba67ba25a9bc335f6d748adc2f
Jinyoung Eom <jinyoung.eom@gmail.com>
2016-04-04 17:40:13 +0900

    .

--- web01/src/main/java/Hello.java
+++ web01/src/main/java/Hello.java
@@ -5,21 +5,22 @@ public class Hello {
 	
 	system.out.println("감자먹고싶다.");
 
-	System.out.println("calculate a+b ::");
+	System.out.println("calculatesdds a+b ::");
 	int a= 20;
 	int b= 10;
+	System.out.println("okoko");
 	
 	int c=a+b;
-	System.out.println(c+"==>donedddddddd!");
+	System.out.println(c+"==>ddddeonedddddddd!");
 	
 	int age = 20;
 	if (age < 20) {
-		System.out.println("오호라.. ");
+		System.out.println("오dsds호라.. ");
 	} else {
-		System.out.println("우헤헤...");
+		System.out.println("우sdds헤헤...");
 	}
 	
 	
-	system.out.println("수정_토마스");
+	system.out.println("수정_dsds토마스");
   }
 }
