8102a7385099d265f6f62aaea63c6f6a6846555a
Jinyoung Eom <jinyoung.eom@gmail.com>
2016-04-04 17:25:49 +0900

    새 버전과 또 합쳤다.

--- web01/src/main/java/Hello.java
+++ web01/src/main/java/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
   public static void main(String[] args) {
 	System.out.println("okok");
 	System.out.println("dd");
+	
 	system.out.println("감자먹고싶다.");
 
 	System.out.println("calculate a+b ::");
@@ -10,5 +11,13 @@ public class Hello {
 	
 	int c=a+b;
 	System.out.println(c+"==>done!");
+	
+	int age = 20;
+	if (age < 20) {
+		System.out.println("오호라.. ");
+	} else {
+		System.out.println("우헤헤...");
+	}
+	
   }
 }
